b466557c287daebc80c6880b78469d11d8aaadec
edgardamian <edgar.mora.d@gmail.com>
2025-05-13 23:05:57 -0600

    Update script.js

--- 2/script.js
+++ 2/script.js
@@ -1,8 +1,6 @@
 
 
-const map = L.map('map').setView([-16.510617, -68.172808
-
-], 8)
+const map = L.map('map').setView([19.702843, -101.192327], 8)
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
